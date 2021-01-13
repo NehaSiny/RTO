6699e3edb50233bfe1a32b12c02604123e77bc3a
NehaSiny <77193317+NehaSiny@users.noreply.github.com>
2021-01-13 22:46:46 +0400

    Update RTAOFF.py

--- RTAOFF.py
+++ RTAOFF.py
@@ -3,9 +3,9 @@ import pandas as pd
 import seaborn as sns
 
 st.balloons()
+#https://kerala.data.gov.in/catalog/motor-vehicles-department-kerala#web_catalog_tabs_block_10 
 df = pd.read_csv('/Users/nirmal/Desktop/RTO.csv')
 st.title("Kerala Road Transport Authority ")
-#https://kerala.data.gov.in/catalog/motor-vehicles-department-kerala#web_catalog_tabs_block_10 
 option1 = st.selectbox('Select District ',df['Office'].unique())
 kk=df[df.Office == option1].sort_values('OfficeType')
 st.write(kk)
